fa4afc842525a6942a72302ff60dfe11928db9ff
Timur <you@example.com>
2022-06-14 17:21:41 -0700

    .

--- Migrations.Dockerfile
+++
@@ -1,8 +0,0 @@
-FROM mcr.microsoft.com/dotnet/sdk:6.0
-RUN dotnet tool install -g dotnet-ef
-ENV PATH $PATH:/root/.dotnet/tools
-
-COPY . /app
-WORKDIR /app
-
-CMD exec /bin/bash -c "trap : TERM INT; sleep infinity & wait"
---
+++ src/Frontend.Data/Migrations/20220614224817_First.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Frontend.Data.Migrations
+{
+    public partial class First : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterDatabase()
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "Articles",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Title = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Articles", x => x.Id);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.InsertData(
+                table: "Articles",
+                columns: new[] { "Id", "Title" },
+                values: new object[] { 1, "Статья первая" });
+
+            migrationBuilder.InsertData(
+                table: "Articles",
+                columns: new[] { "Id", "Title" },
+                values: new object[] { 2, "Статья вторая" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Articles");
+        }
+    }
+}
